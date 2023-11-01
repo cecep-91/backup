ed3c958c5c3e1dec63aa6599a8c87a54dda8ea78
cecep-91 <148958846+cecep-91@users.noreply.github.com>
2023-11-01 17:17:35 +0700

    kyaa~

--- backup.sh
+++ backup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Backup script for Ubuntu by cecep-91
+
 # Check syntax
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 <source_directory> <destination_directory> <backup_name>"
+if [ $# -ne 4 ]; then
+    echo "Usage: $0 <source_directory> <destination_directory> <backup_name> <schedule>"
     exit 1
 fi
 
@@ -27,6 +29,8 @@ else
     backup_name="${3}-${mmddyyyy}"
 fi
 
+schedule=$4
+
 # Check whether user have acces to directories
 ls ${source_directory} && ls ${destination_directory}
 if [ $? -ne 0 ]; then
@@ -43,3 +47,22 @@ fi
 # Start backup
 echo "${source_directory} is being backed up ..."
 tar -czvf ${destination_directory}${backup_name} ${source_directory} >> /dev/null
+
+case "$schedule" in
+    "daily")
+        cron="0 0 * * *"
+        ;;
+    "weekly")
+        cron="0 0 * * 1"
+        ;;
+    "monthly")
+        cron="0 0 1 * *"
+        ;;
+    *)
+        echo "Invalid schedule. Supported values are: daily, weekly, monthly."
+        exit 5
+        ;;
+esac
+
+# Add the cron job
+(crontab -l 2>/dev/null; echo "$cron $PWD/$0 $1 $2 $3") | crontab -
